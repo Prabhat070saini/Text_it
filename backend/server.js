537731c0f7d04031b631985acf2b14158cc45fd8
Paras-cyber <65559478+Paras-cyber@users.noreply.github.com>
2023-04-09 17:22:02 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const app = express();
-app.get("/",(req,res)=>{
-    res.send("Api is ssssssssssssssssssssssssssssssssssssssssss");
-});
-const PORT=process.env.PORT || 5000
-app.listen(5000,console.log(`server start on port ${PORT}`));
