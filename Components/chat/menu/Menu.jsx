7103998211883654cc61a3443438e220a3fde716
Paras-cyber <65559478+Paras-cyber@users.noreply.github.com>
2023-04-09 17:17:31 +0530

    Add files via upload

---
+++ Components/chat/menu/Menu.jsx
@@ -0,0 +1,15 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import Header from './Header'
+import Search from './Search'
+
+function Menu() {
+  return (
+    <div>
+        <Header/>
+        <Search/>
+    </div>
+  )
+}
+
+export default Menu
