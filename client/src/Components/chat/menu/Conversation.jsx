36b8b305ae23f99d3dde8f5e4c4f0846555b6e77
Paras-cyber <65559478+Paras-cyber@users.noreply.github.com>
2023-04-29 16:35:09 +0530

    Add files via upload

---
+++ client/src/Components/chat/menu/Conversation.jsx
@@ -0,0 +1,48 @@
+import { Box , Typography, styled} from '@mui/material'
+import { useContext, useEffect, useState } from 'react';
+import { AccountContext } from '../../../context/AccountProvider';
+import { setConversation } from '../../../service/api';
+import { UserContext } from '../../../context/UserProvider';
+
+const Component = styled(Box)`
+    height: 45px;
+    display: flex;
+    padding: 13px 0;
+    cursor: pointer;
+`;
+   
+const Image = styled('img') ({
+  width: 50,
+  height: 50,
+  objectFit: 'cover',
+  borderRadius: '50%',
+  padding: '0 14px'
+});
+
+
+function Conversation({user}) {
+
+  const { setPerson , account} = useContext(AccountContext);
+
+  const getUser = async () => {
+    setPerson(user);
+    await setConversation({senderId: account.sub, receiverId: user.sub})
+  }
+
+  return (
+    <Component onClick={()=> getUser()}>
+      <Box>
+        <Image src={user.picture} alt="dp" />
+      </Box>
+      <Box>
+        <Box>
+          <Typography>
+            {user.name}
+          </Typography>
+        </Box>
+      </Box>  
+    </Component>
+  )
+}
+
+export default Conversation
